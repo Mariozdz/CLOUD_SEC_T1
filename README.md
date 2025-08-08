e57ca0f6f68c1a575e1d051e23d0e72aa7c0fffd
Mario Arguello Borge <46728059+Mariozdz@users.noreply.github.com>
2025-08-07 21:33:35 -0600

    feat: tarea 1 - adding new relations in diagram

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CLOUD_SEC_T1
 Tarea 1 del curso de seguridad en la nube - Maestría en Ciberseguridad 
+
+Resultado temporal ![Logo del proyecto](digital_identity.png)
--- tarea_1.py
+++ tarea_1.py
@@ -1,4 +1,4 @@
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.network import Route53, CloudFront, APIGateway
 from diagrams.aws.security import WAF
 from diagrams.k8s.compute import Pod
@@ -16,6 +16,18 @@ with Diagram("Digital Identity", show=False):
     # dns_zones = [Route53("DNS Zones") for _ in range(4)]
     # dns_policy = Route53("DNS Security Policy")
 
+    with Cluster("DNS"):
+        dns_zone_1 = Route53("DNS Zone 1")
+        dns_zone_2 = Route53("DNS Zone 2")
+        dns_zone_3 = Route53("DNS Zone 3")
+        #dns_zones = [Route53("DNS Zones") for _ in range(3)]
+        dns_policy = Route53("DNS Security Policy")
+
+    with Cluster("Legacy services"):
+        legacy_issuer = Server("Legacy Issuer")
+        legacy_verifier = Server("Legacy Verifier")
+        legacy_issuer - legacy_verifier
+
     # Presentation Layer
     with Cluster("VPC"):
 
@@ -27,13 +39,17 @@ with Diagram("Digital Identity", show=False):
         with Cluster("Kubernets: Application Layer"):
             
             # kube = ETCD("K8S")
+            ingresController = Ingress("Ingress Controller")
 
             with Cluster("Namespace: Wallet services", direction="TB"):
-                ingres = Ingress("Ingress")
+                ingress = Ingress("Ingress")
+
                 wallet = Pod("Wallet service")
                 capp = Pod("cApp Service")
-                user = Pod("User service")
                 cert_vol = PV("Volume") 
+            
+            with Cluster ("Namespace: User services"):
+                user = Pod("User service")
             
             with Cluster("Namespace: Verifier services"):
                 verifier = Pod("Verifier service")
@@ -52,9 +68,28 @@ with Diagram("Digital Identity", show=False):
             db = PostgreSQL("Amazon RDS")
             blockchain = Server("HyperLedger Fabric")  # placeholder
 
-    api_gateway >> ingres
-    ingres >> capp
+    # Connections based on edges with transparent color are not valid - related
+
+    issuer >> legacy_issuer
+    verifier >> legacy_verifier
+
+    dns_policy >> Edge(color="transparent") >> dns_zone_1
+    dns_policy >> Edge(color="transparent") >> dns_zone_2
+    dns_policy >> Edge(color="transparent") >> dns_zone_3
+    dns_zone_3 >> Edge(color="transparent") >> api_gateway
+    api_gateway >> ingresController
+    ingresController >> ingress
+    ingress >> capp
+    capp >> Edge(label="Read blockchain") >> user
+    user >> Edge(label="Read") >> blockchain
+    cert_vol >> Edge(color="transparent") >> capp
+    capp >> verifier
+    capp >> wallet
+    capp  >> trust_agent
+    wallet >> blockchain
     wallet >> issuer
     wallet >> verifier
-    notification_service >> trust_agent
-    cert_vol >> notification_service
+    # notification_service >> trust_agent
+    issuer >> Edge(color="transparent") >> trust_agent
+    trusted_mail_service >> Edge(color="transparent") >> notification_service
+    notification_service >> Edge(color="transparent") >> db
